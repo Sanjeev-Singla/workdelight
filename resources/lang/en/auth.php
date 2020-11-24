eb9243d9855bd2403643f8afad458e09edfad56a
sanjeev <sfs.sanjeev20@gmail.com>
2020-11-24 17:18:57 +0530

    initial commit

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'error.password' => 'The provided password is incorrect.',
+    'password' => 'Enter Password',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'sign_in'   => 'SignIn',
+    'login.title' => 'Login',
+    'email' => 'Enter Valid Email',
+    'remember_me' => 'Remember Me',
+    'login.forgot_password' => 'Forgot Password',
+    'login.register_membership' => 'Register Now',
+    'full_name' => 'Enter Full Name',
+    'confirm_password' => 'Enter Confirm Password',
+    'registration.i_agree' => 'I Agree',
+    'registration.terms' => 'Terms & Condtions',
+    'registration.have_membership' => 'Already Registered',
+    'register' => 'Register',
+    'registration.title' => 'Registration',
+    'app.search' => 'Search'
+];
